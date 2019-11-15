6b8eaf0c66da28a1e9e72f10b9e5309f3ce51146
raizo <35413558+iraizo@users.noreply.github.com>
2019-11-15 19:07:04 +0100

    fakl

--- upload.php
+++ upload.php
@@ -16,6 +16,8 @@ class dbsettings { // you need to change all of this
     public static $db = "sharex";
 }
 
+
+
 function generateRandomString($length) {
     $characters = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
     $charactersLength = strlen($characters);
@@ -29,7 +31,6 @@ function generateRandomString($length) {
 $connection = new mysqli(dbsettings::$server, dbsettings::$user, dbsettings::$pass, dbsettings::$db)
     OR die(mysqli_error());
 
-
 function getuserinfo($connection, $db, $uid, $row) {
     $query = "SELECT * FROM $db WHERE UID=$uid";
     $result = $connection->query($query);
@@ -37,53 +38,27 @@ function getuserinfo($connection, $db, $uid, $row) {
     $information = $rowresult[$row];
     return $information;
 }
-function password_array($connection, $db) {
+function token_array($connection, $db) {
     $query = "SELECT `UserPassword` FROM `sharex`";
     $result = $connection->query($query);
-    $password_array = array();
+    $token_array = array();
 
     while($row = mysqli_fetch_array($result)) {
-        //$password_array[] = "\"".$row['UserPassword']."\"";
-        $password_array[] = $row['UserPassword'];
+        print($row['UserPassword']);
+        $token_array[] = $row['UserPassword'];
     }
-    // not really needed just for the future TODO: REMOVE THIS
-    $password_int = implode(",", $password_array);
-    return $password_array;
+    return $token_array;
 }
 
+$tokenlist = token_array($connection, dbsettings::$db);
 if(isset($_POST['token'])) {
-    if(in_array($_POST['token'], password_array($connection, dbsettings::$db))) {
-        if($randomstring) {
-            $file = generateRandomString($length);
-            $target = $_FILES["sharex"]["name"];
-            $type = pathinfo($target, PATHINFO_EXTENSION);
-
-            if(move_uploaded_file($_FILES["sharex"]["tmp_name"], $dir.file.'.'.$type)) {
-                $output = $url . "/" . $dir . $file . "." . $type; //TODO : RETURN TO CLIENT
-            } else {
-                echo "Could not upload file to $dir";
-            }
-        }
-        else {
-            // TODO: know how to get filename from sharex
-            /*
-            $target = explode(".", $_FILES["sharex"]["name"];
-            $type = pathinfo($target, PATHINFO_EXTENSION);
-            if(move_uploaded_file($_FILES["sharex"]["tmp_name"], $dir.file.".".$type)) {
-                $output = $url . "/" . $dir . $target . "." . $type;
-            } else {
-                echo "Could not upload file to $dir";
-            }
-            */
-        }
-    } else {
-        echo "Wrong token";
+    if (in_array($_POST["token"], $tokenlist)) {
+        echo "token is in array";
     }
-} else {
-    echo "No data received from client.";
+    else {
+        echo "not in array";
+        echo " Request: " .$_POST['token'] . " Array: " . $tokenlist;
+    }
 }
-
 $connection->close();
 ?>
-
-
