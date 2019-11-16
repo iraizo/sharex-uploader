ff937e18f554f5ff2aa80c90e4fe77430d9e3417
raizo <35413558+iraizo@users.noreply.github.com>
2019-11-16 01:54:42 +0100

    1cock fix plz

--- config.php
+++ config.php
@@ -1,7 +1,14 @@
 <?php
 // TODO: EXPAND AND PUT EVERYTHING INTO HERE INSTEAD OF UPLOAD.PHP
 return array(
-    'url' => 'https://raizo.de/'
+    'url' => 'https://raizo.de/',
+    'db' => 'sharex',
+    'server' => 'localhost',
+    'user' => 'sharexadmin',
+    'pass' => 'tessa',
+    'directory' => 'p/',
+    'randomstringlength' => 5,
+    'randomstring' => true,
 )
 
 
