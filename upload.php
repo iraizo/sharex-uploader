a02c12ec45789ce9050b7838b02aa17bdaf4d1f1
raizo <35413558+iraizo@users.noreply.github.com>
2019-11-16 16:42:46 +0100

    gay

--- upload.php
+++ upload.php
@@ -14,21 +14,6 @@ $length = $config['randomstringlength'];
 $randomstring = $config['randomstring'];
 
 try {
-$conndata = "mysql:host=$server;dbname=$db";
-$connection = new PDO($conndata, $user, $pass);
-$connection->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-} catch(PDOException $e) {
-	echo $e->getMessage();
-	die();
-}
-
-
-function generateRandomString($length) {
-    $characters = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
-    $charactersLength = strlen($characters);
-    $randomString = '';
-    for ($i = 0; $i < $length; $i++) {
-        $randomString .= $characters[rand(0, $charactersLength - 1)];
     $connection = new PDO("mysql:host=localhost;dbname=sharex", "sharexadmin", "tessa"); //TODO USE THIS WITH CONFIGS
     $connection->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
@@ -45,36 +30,23 @@ function generateRandomString($length) {
 
     function getuserinfo($connection, $db, $uid, $row)
     {
+
         $stmt = $connection->prepare("SELECT * FROM $db WHERE UID=$uid");
         $stmt->execute();
-        $result = $stmt->fetchAll(\PDO::FETCH_ASSOC);
-        $information = $result[$row];
-        return $information;
+        $row = $stmt->fetch(PDO::FETCH_ASSOC);
+        return $row["amount"] > 0;
     }
-      
-// I don't really know if this function works
-function getuserinfo($connection, $db, $uid, $row) {
-    $query = $connection->prepare("SELECT * FROM ? WHERE UID = ?");
-    $result = $query->execute(array($db, $uid));
-    $rowresult = $query->fetchAll();
-    return $rowresult;
-}
+
     function TokenExists(string $token, $connection)
     {
         $stmt = $connection->prepare('SELECT COUNT(UserPassword) AS amount FROM sharex WHERE UserPassword = "' . $token . '"');
         $stmt->execute();
-        $result = $stmt->fetchAll(\PDO::FETCH_ASSOC);
+        $result = $stmt->fetchAll(PDO::FETCH_ASSOC);
         return $result["amount"] > 0;
     }
 
 
-
-    $query = $connection->prepare('SELECT COUNT(UserPassword) FROM sharex WHERE UserPassword = "?"');
-    $result = $query->execute(array($token));
-    $row = $query->fetchAll();
-    return $row > 0;
-}
-if (isset($_POST['token'])) {
+    if (isset($_POST['token'])) {
         if (TokenExists($_POST['token'], $connection)) {
             if ($randomstring) {
                 $filename = generateRandomString($length); // TODO MOVE THIS SO I DONT NEED TO REPEAT CODE
@@ -86,23 +58,19 @@ if (isset($_POST['token'])) {
                 } else {
                     echo "Possible permission error contact the server administrator.";
                 }
+
+
             } else {
                 // TODO: get file name from sharex and dont use generateRandomString
             }
+        } else {
+            echo "Wrong Token contact the server administrator.";
+        }
     } else {
-        echo "Wrong Token\nContact server administrator";
-    }
-} else {
-    echo "No POST data received from client.";
-}
-
-//remove the connection and unset the db credentials AND config variable
-$connection = null;
-
-unset($config, $db, $server, $user, $pass);
         echo "No post data received from client.";
     }
 } catch(PDOException $e)
 {
     echo "PDO Error: " . $e->getMessage();
 }
+$connection = null;
